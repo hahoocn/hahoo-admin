d829bcd857a53dd30365e240c510a77bbee95cc4
Ray Guo <ihahoo@163.com>
2016-08-07 13:27:28 +0800

    更新测试api

--- api/server.js
+++ api/server.js
@@ -39,13 +39,25 @@ app.post('/scml/order', (req, res) => {
 app.get('/scml/details', (req, res) => {
   setTimeout(() => {
     res.send({
-      id: req.query.id,
+      id: parseInt(req.query.id, 10),
       title: '这是内容标题的测试，标题长一点会换行，那么就测试看看标题长长的样子',
       updateTime: '2016-10-23',
       orderId: 99,
       isPublish: 1,
       info: '这个是简介内容，内容长一点呀长一点。这个是简介内容，内容长一点呀长一点。这个是简介内容，内容长一点呀长一点。'
     });
+  }, 500);
+});
+
+app.post('/scml/add', (req, res) => {
+  setTimeout(() => {
+    res.send({ response: 'success' });
+  }, 500);
+});
+
+app.post('/scml/update', (req, res) => {
+  setTimeout(() => {
+    res.send({ response: 'success' });
   }, 500);
 });
 
